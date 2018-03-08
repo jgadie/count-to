9d534131572a176c4c8c39b234638c02a39ee108
Jasmine Gadie and Megan Lee <jasmine@>
2018-03-07 20:59:32 -0800

    removed css to display multliples on webpage

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,14 @@ $(document).ready(function() {
     var number2 = parseInt($("input#number2").val());
     var multiples = [];
 
-  for (var i = number2; i <= number; i += number2) {
-    if (number % i === 0) {
-    multiples.push(i);
-  }
+      for (var i = number2; i <= number; i += number2) {
+        if (number % i === 0) {
+          multiples.push(i);
+        };
+
+    $('#result').text(multiples);
 
 
-    $("#result").text(multiples);
 
   };
  });
